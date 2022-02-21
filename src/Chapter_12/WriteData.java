302aa3e34b5a87b6cabd4d4ff0ff697fe8bb7c08
Timothy <76513057+TimG27@users.noreply.github.com>
2022-02-21 23:03:00 +0530

    Excep & File
    Added exceptions and file writing

---
+++ src/Chapter_12/WriteData.java
@@ -0,0 +1,26 @@
+package Chapter_12;
+
+import java.io.IOException;
+
+public class WriteData {
+    public static void main(String[] args) throws IOException {
+        java.io.File file = new java.io.File ("scores.txt");
+
+        if (file.exists()) {
+            System.out.println("File already exist");
+            System.exit (1);
+        }
+
+        try (
+        java.io.PrintWriter output = new java.io.PrintWriter (file);
+        )
+
+        {
+        output.print("John T Smith ");
+        output.print(90);
+        output.print("Eric K Jones ");
+        output.println(85);
+        }
+       // output.close();
+    }
+}
